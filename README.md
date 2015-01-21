2e6fc948ca7c1da7728a29631b073b24f69eb470
Alex Baulch <abaulch@thisiszone.com>
2015-01-21 17:53:59 +0000

    adding JSDoc and updating the README

---
+++ README.md
@@ -0,0 +1,33 @@
+#CommonJS Pub Sub
+A very small pub sub library that whole-heartedly stole all it's ideas (and a good chunk of code) from David Walsh. It has been upated so that it will work all the way down to IE8, hence the lack of ```hasOwnProperty```. You can find the original code [here](http://davidwalsh.name/pubsub-javascript).
+
+##Install
+```npm install common-js-pub-sub```
+
+##Use with Browserify
+```javascript
+var events = require('common-js-pub-sub')
+```
+
+##Publish to a topic
+```javascript
+events.publish('/page/load', {
+	// doesn't have to be a url any key value pairs can be passed here
+	url: '/some/url/path'
+});
+```
+
+##Subscribe to a topic
+```javascript
+var subscription = events.subscribe('/page/load', function(obj) {
+	// Do something now that the event has occurred
+	
+	// Note 'obj' represents the object you passed to the publish function
+});
+```
+
+##Remove said topic
+```javascript
+// Toodles
+subscription.remove();
+```
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 'use strict';
 
+/**
+ * Minature pub sub module for working with pub sub in a common js pattern
+ * @module common-js-pub-sub
+ * @returns { Object } events
+ * @returns { function } events.publish
+ * @returns { function } events.subscribe
+ */
 module.exports = function() { 
  
     var topics = {};
 
+    /**
+     * Subscribe to a topic
+     * @param  { string } topic - Name of the topic to subscribe to
+     * @param  { function } listener - Callback to be run when the topic is published
+     * @returns {[type]}
+     */
     function subscribe(topic, listener) {
         // Create the topic's object if not yet created
         if (!topics[topic]) {
@@ -17,19 +30,27 @@ module.exports = function() {
 
         // Provide handle back for removal of topic
         return {
+            /**
+             * removes the topic from the queue
+             */
             remove: function() {
                 delete topics[topic].queue[index];
             }
         };
     }
 
+    /**
+     * Publish a topic
+     * @param  { string } topic - Name of the topic to be published
+     * @param  { Object } info - Object containing any additional information you want available to the subscribe function
+     */
     function publish(topic, info) {
         // If the topic doesn't exist, or there's no listeners in queue, just leave
         if (!topics[topic] || !topics[topic].queue.length) {
             return;
         }
 
-        // Cycle through topics queue, fire!
+        // Cycle through topics queue and call the function!
         var items = topics[topic].queue;
         items.forEach(function(item) {
             item(info || {});
